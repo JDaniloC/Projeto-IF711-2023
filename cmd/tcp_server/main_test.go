03a4ad50bcaa3ec3a5964c3d9b46fa9b6258bfd7
José Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2024-02-02 18:14:59 +0000

    Feat: add benchmark test to TCP server

---
+++ cmd/tcp_server/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"net"
+	"testing"
+
+	tcp "github.com/JDaniloC/Projeto-IF711-2023/pkg/server"
+)
+
+func init() {
+	tcpServer := tcp.NewTCPServer(":1123")
+	go tcpServer.Start()
+}
+
+func BenchmarkTCPServer(b *testing.B) {
+	request := map[string]interface{}{
+		"link":  "https://hackerspaces.org/",
+		"depth": 1,
+	}
+	req, err := json.Marshal(request)
+	if err != nil {
+		b.Error("Error on serialize request", err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		conn, err := net.Dial("tcp", ":1123")
+		if err != nil {
+			b.Error("could not connect to server: ", err)
+		}
+
+		_, err = conn.Write(append(req, '\n'))
+		if err != nil {
+			b.Error("could not write payload to server: ", err)
+		}
+
+		reader := bufio.NewReader(conn)
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			b.Error("Error on read server answer:", err)
+		}
+
+		var response map[string]interface{}
+		err = json.Unmarshal([]byte(s), &response)
+		if err != nil {
+			b.Error("Could not unmarshal response", err)
+		}
+
+		errMsg, hasError := response["error"].(string)
+		if hasError {
+			b.Error(errMsg)
+		}
+
+		conn.Close()
+	}
+}
