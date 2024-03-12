16283d7781c4e57212a4bae414811aec0d441140
José Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2024-03-12 20:26:11 +0000

    RabbitMQ producer and consumer added

--- pkg/server/rabbitmq_server.go
+++ pkg/server/rabbitmq_server.go
@@ -1,86 +1,128 @@
 package server
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "net/rpc"
+	"encoding/json"
+	"fmt"
+	"log"
 
-    "github.com/JDaniloC/Projeto-IF711-2023/pkg/runner"
-    "github.com/streadway/amqp"
+	"github.com/JDaniloC/Projeto-IF711-2023/pkg/runner"
+	"github.com/streadway/amqp"
 )
 
-type CrawlerRabbitMQ struct{}
+type CrawlerAMQP = CrawlerRPC
 
-type RequestMQ struct {
-    Link  string
-    Depth int
+type RabbitMQ struct {
+	addr     string
+	channel  *amqp.Channel
+	rabbitMQ *amqp.Connection
+	request  amqp.Queue
+	response amqp.Queue
 }
 
-type ResponseMQ struct {
-    ValidLinks   []string
-    InvalidLinks []string
+func (r *RabbitMQ) processRequest(request *Request) *Response {
+	controller := runner.TimeoutCrawl(request.Link, request.Depth)
+	return &Response{
+		ValidLinks:   controller.ValidLinks.ToArray(),
+		InvalidLinks: controller.InvalidLinks.ToArray(),
+	}
 }
 
-func (c *CrawlerRabbitMQ) Crawl(request *RequestMQ, response *ResponseMQ) error {
-    controller := runner.TimeoutCrawl(request.Link, request.Depth)
-    response.ValidLinks = controller.ValidLinks.ToArray()
-    response.InvalidLinks = controller.InvalidLinks.ToArray()
-    return nil
+func (r *RabbitMQ) Start() (err error) {
+	r.rabbitMQ, err = amqp.Dial(r.addr)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to serve RabbitMQ", err)
+		return err
+	}
+
+	r.channel, err = r.rabbitMQ.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+		return err
+	}
+
+	r.request, err = r.channel.QueueDeclare(
+		"request", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	r.response, err = r.channel.QueueDeclare(
+		"reponse", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	fmt.Println("Listening RPC server at", r.addr)
+	return nil
 }
 
-type RabbitMQServer struct {
-    addr     string
-    rabbitMQ *amqp.Connection
+func (r *RabbitMQ) Serve() {
+	msgs, err := r.channel.Consume(
+		r.request.Name, // queue
+		"",             // consumer
+		true,           // auto-ack
+		false,          // exclusive
+		false,          // no-local
+		false,          // no-wait
+		nil,            // args
+	)
+	if err != nil {
+		log.Printf("Failed to register a consumer: %v", err)
+		return
+	}
+
+	for d := range msgs {
+		var request Request
+		if err := json.Unmarshal(d.Body, &request); err != nil {
+			log.Printf("failed to decode message: %v", err)
+			continue
+		}
+
+		// Process the request
+		response := r.processRequest(&request)
+		bytes, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("failed to mashal response: %v", err)
+			continue
+		}
+
+		// Send the response back
+		if err := r.channel.Publish(
+			"",              // exchange
+			r.response.Name, // routing key
+			false,           // mandatory
+			false,           // immediate
+			amqp.Publishing{
+				ContentType:   "application/json",
+				CorrelationId: d.CorrelationId,
+				Body:          bytes,
+			}); err != nil {
+			log.Printf("failed to publish a response: %v", err)
+			continue
+		}
+	}
 }
 
-func (r *RabbitMQServer) Start() (err error) {
-    crawler := new(CrawlerRabbitMQ)
-    if err := rpc.Register(crawler); err != nil {
-        return fmt.Errorf("error registering service: %s", err)
-    }
-
-    http.HandleFunc("/rpc", r.handleRPCRequest)
-
-    r.rabbitMQ, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
-    if err != nil {
-        return fmt.Errorf("failed to connect to RabbitMQ: %v", err)
-    }
-    defer r.rabbitMQ.Close()
-
-    fmt.Println("Listening RPC server at", r.addr)
-    log.Fatal(http.ListenAndServe(r.addr, nil))
-    return nil
+func (r *RabbitMQ) Close() {
+	r.channel.Close()
+	r.rabbitMQ.Close()
 }
 
-func (r *RabbitMQServer) handleRPCRequest(w http.ResponseWriter, req *http.Request) {
-    var request RequestMQ
-    var response ResponseMQ
-
-    err := json.NewDecoder(req.Body).Decode(&request)
-    if err != nil {
-        http.Error(w, "failed to decode request", http.StatusBadRequest)
-        return
-    }
-
-    crawler := new(CrawlerRabbitMQ)
-    err = crawler.Crawl(&request, &response)
-    if err != nil {
-        http.Error(w, "failed to process request", http.StatusInternalServerError)
-        return
-    }
-
-    err = json.NewEncoder(w).Encode(&response)
-    if err != nil {
-        http.Error(w, "failed to encode response", http.StatusInternalServerError)
-        return
-    }
-}
-
-func NewRabbitMQServer(addr string) *RabbitMQServer {
-    server := RabbitMQServer{
-        addr: addr,
-    }
-    return &server
+func NewRabbitMQ(addr string) *RabbitMQ {
+	server := RabbitMQ{
+		addr: addr,
+	}
+	return &server
 }
--- pkg/server/rabbitmq_server_test.go
+++ pkg/server/rabbitmq_server_test.go
@@ -3,122 +3,78 @@ package server
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"testing"
 
-	"github.com/JDaniloC/Projeto-IF711-2023/pkg/runner"
 	"github.com/streadway/amqp"
 )
 
 const (
 	rabbitMQAddr = "amqp://guest:guest@localhost:5672/"
-	queueName    = "rpc_requests"
+	queueName    = "crawl"
 )
 
-type RequestMQTest struct {
-	Link  string
-	Depth int
-}
-
-type ResponseMQTest struct {
-	ValidLinks   []string
-	InvalidLinks []string
-}
-
-type RabbitMQServerMQTest struct {
-	addr    string
-	channel *amqp.Channel
-}
-
-func NewRabbitMQServerMQTest(addr string) *RabbitMQServerMQTest {
-	return &RabbitMQServerMQTest{
-		addr: addr,
-	}
-}
-
-func (r *RabbitMQServerMQTest) StartMQ() error {
-	rabbitConn, err := amqp.Dial(rabbitMQAddr)
-	if err != nil {
-		return fmt.Errorf("failed to connect to RabbitMQ: %v", err)
-	}
-	defer rabbitConn.Close()
-
-	channel, err := rabbitConn.Channel()
-	if err != nil {
-		return fmt.Errorf("failed to open a channel: %v", err)
-	}
-	defer channel.Close()
-	r.channel = channel
-
-	queue, err := channel.QueueDeclare(
-		queueName, // name
-		false,     // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		nil,       // arguments
-	)
-	if err != nil {
-		return fmt.Errorf("failed to declare a queue: %v", err)
-	}
-
-	msgs, err := channel.Consume(
-		queue.Name, // queue
-		"",         // consumer
-		true,       // auto-ack
-		false,      // exclusive
-		false,      // no-local
-		false,      // no-wait
-		nil,        // args
-	)
-	if err != nil {
-		return fmt.Errorf("failed to register a consumer: %v", err)
-	}
-
-	forever := make(chan bool)
+func init() {
+	amqpServer := NewRabbitMQ(rabbitMQAddr)
 
 	go func() {
-		for d := range msgs {
-			var request RequestMQTest
-			if err := json.Unmarshal(d.Body, &request); err != nil {
-				log.Printf("failed to decode message: %v", err)
-				continue
-			}
-
-			// Process the request
-			response := r.processRequest(&request)
-
-			// Send the response back
-			if err := r.channel.Publish(
-				"",          // exchange
-				d.ReplyTo,   // routing key
-				false,       // mandatory
-				false,       // immediate
-				amqp.Publishing{
-					ContentType:   "application/json",
-					CorrelationId: d.CorrelationId,
-					Body:          responseToBytes(response),
-				}); err != nil {
-				log.Printf("failed to publish a response: %v", err)
-				continue
-			}
-		}
+		amqpServer.Start()
+		amqpServer.Serve()
 	}()
-
-	fmt.Printf("Listening RPC server at %s\n", r.addr)
-	<-forever
-
-	return nil
 }
 
-func responseToBytes(response *ResponseMQTest) []byte {
-	bytes, _ := json.Marshal(response)
-	return bytes
-}
-
-func (r *RabbitMQServerMQTest) processRequest(request *RequestMQTest) *ResponseMQTest {
-	controller := runner.TimeoutCrawl(request.Link, request.Depth)
-	return &ResponseMQTest{
-		ValidLinks:   controller.ValidLinks.ToArray(),
-		InvalidLinks: controller.InvalidLinks.ToArray(),
+func TestAMQPServer_Connection(t *testing.T) {
+	conn, err := amqp.Dial(rabbitMQAddr)
+	if err != nil {
+		t.Errorf("failed to connect to RabbitMQ: %v", err)
 	}
+	defer conn.Close()
+}
+
+func TestAMQPServer_Request(t *testing.T) {
+	request := &Request{
+		"https://hackerspaces.org/", 1,
+	}
+	bytes, err := json.Marshal(request)
+	if err != nil {
+		t.Errorf("filed to mashall request: %v", err)
+	}
+
+	amqpClient := NewRabbitMQ(rabbitMQAddr)
+	err = amqpClient.Start()
+	if err != nil {
+		t.Errorf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer amqpClient.Close()
+
+	msgs, err := amqpClient.channel.Consume(
+		amqpClient.response.Name, // queue
+		"",                       // consumer
+		true,                     // auto-ack
+		false,                    // exclusive
+		false,                    // no-local
+		false,                    // no-wait
+		nil,                      // args
+	)
+	if err != nil {
+		t.Errorf("failed to register a consumer: %v", err)
+	}
+
+	t.Run("Send AMQP request", func(t *testing.T) {
+		if err := amqpClient.channel.Publish(
+			"",                      // exchange
+			amqpClient.request.Name, // routing key
+			false,                   // mandatory
+			false,                   // immediate
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        bytes,
+			}); err != nil {
+			t.Errorf("failed to publish the request: %v", err)
+		}
+
+		for d := range msgs {
+			fmt.Printf("Received a message: %s", d.Body)
+			break
+		}
+	})
 }
